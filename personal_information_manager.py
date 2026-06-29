cdcc9f1f60ebb92e5f4bb0f4f869a4f89e7e1338
Ashwanth Kola <ashwanthkola7@gmail.com>
2026-06-29 18:13:48 +0530

    Add personal information section

---
+++ personal_information_manager.py
@@ -0,0 +1,16 @@
+# ==========================================
+# Personal Information Manager
+# Mini Project 01
+# ==========================================
+
+print("========== PERSONAL INFORMATION =========")
+
+name = "Ashwanth"
+age = 20
+city = "Hyderabad"
+occupation = "Student"
+
+print("Name       :", name)
+print("Age        :", age)
+print("City       :", city)
+print("Occupation :", occupation)
